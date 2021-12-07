4ff37693dd8856841b3d835d1337fc4d4a3c2c48
Jade <jaderichard@live.fr>
2021-12-07 12:04:44 +0100

    exercice_4

---
+++ VARIABLES/Exercice_4/public/js/main.js
@@ -0,0 +1,22 @@
+/* let name = prompt("Name");
+
+alert("Bonjour, " +name); */
+
+/* let age = prompt("Age");
+
+alert("Tu as " +age +" ans");
+ */
+
+/* let firstname = prompt(" First Name ");
+
+let lastname = prompt(" Last Name ");
+
+let fullname = firstname + lastname
+
+alert( fullname ); */
+
+let firstname = prompt("First Name ");
+
+let lastname = prompt("Last Name "); 
+
+alert( lastname.toUpperCase() + firstname );
