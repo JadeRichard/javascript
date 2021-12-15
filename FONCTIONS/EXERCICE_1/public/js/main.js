85601bc344abc0d3f2fb58c6cd2bca308e4d3dea
Jade <jaderichard@live.fr>
2021-12-15 11:21:03 +0100

    ex1_boucles_while

---
+++ FONCTIONS/EXERCICE_1/public/js/main.js
@@ -0,0 +1,23 @@
+/* //1
+
+const addition = (nb1, nb2) => {
+    let result = nb1 + nb2 
+    console.log(result)
+    return result
+    
+}
+
+addition(1,4); */
+
+/* //2
+
+const soustraction = (nb1, nb2) => {
+    let result = nb1 - nb2 
+    console.log(result)
+    return result
+    
+}
+
+soustraction(42,23); */
+
+//3
