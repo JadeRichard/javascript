a84000c75757b3dcf3310eefc5f9d513d1c96059
Jade <jaderichard@live.fr>
2022-01-18 14:31:00 +0100

    btn_clear

--- cs-miniprojet-todolist/public/js/_variables.js
+++ cs-miniprojet-todolist/public/js/_variables.js
@@ -1,4 +1,5 @@
 export let input_task = document.getElementsByTagName("input")[0]
 export let enter = document.querySelector(".enter")
 export let tasks = document.querySelector(".tasks")
+export let clear = document.querySelector(".clear")
 
--- cs-miniprojet-todolist/public/js/main.js
+++ cs-miniprojet-todolist/public/js/main.js
@@ -1,11 +1,17 @@
 import {enter} from "./_variables.js"
+import {clear} from "./_variables.js"
 import { input_task } from "./_variables.js";
 import { tasks } from "./_variables.js";
 
 enter.addEventListener("click", () =>{
-    console.log(input_task.value);
     let task = document.createElement("input")
     task.value = input_task.value
     tasks.appendChild(task)
     input_task.value = ""
+})
+
+clear.addEventListener("click", () =>{
+    for(let i = 0; tasks.children.length > 0; i++){
+        tasks.removeChild(tasks.lastChild)
+    }
 })
