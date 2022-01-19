a030fc5072e5aedbf3d6e2ae48843bc9167481fa
Jade <jaderichard@live.fr>
2022-01-19 10:04:29 +0100

    filter_btndblclick

--- cs-miniprojet-todolist/public/js/_variables.js
+++ cs-miniprojet-todolist/public/js/_variables.js
@@ -3,18 +3,25 @@ export let enter = document.querySelector(".enter")
 export let tasks = document.querySelector(".tasks")
 export let clear = document.querySelector(".clear")
 export let mode = document.querySelector(".mode")
-export let all = document.querySelector(".all")
+export let all = document.querySelector("button.all")
+export let todo = document.querySelector("button.todo")
+export let done = document.querySelector("button.done")
+
 
 
 export let create_task = () =>{
     let new_task = document.createElement("div")
+    new_task.classList.add("class", "ttodo")
     let nt_p = document.createElement("p")
     let nt_check = document.createElement("input")
     let nt_delete = document.createElement("button")
     let nt_modify = document.createElement("button")
+
+
     nt_p.innerText = input_task.value
     
     nt_check.setAttribute("type", "checkbox")
+    nt_check.setAttribute("class", "checks")
     nt_delete.setAttribute("id", "buttond")
     nt_modify.setAttribute("id", "buttonm")
     nt_delete.className = "delete_yourself"
@@ -27,6 +34,8 @@ export let create_task = () =>{
     new_task.appendChild(nt_p)
     new_task.appendChild(nt_modify)
     new_task.appendChild(nt_delete)
+
+    
     
     nt_delete.appendChild(ntd_text)
     nt_modify.appendChild(ntm_text)
@@ -35,19 +44,64 @@ export let create_task = () =>{
     tasks.appendChild(new_task)
     input_task.value = ""
     
-
-    
-
     let delete_yourself = document.getElementsByClassName("delete_yourself");
     for (let i = 0; i < delete_yourself.length; i++) {
         delete_yourself[i].onclick = function() {
             if(confirm("Do you really want to delete this task ?")){
         let this_task = this.parentElement;
         this_task.style.display = "none";
+        }
     }
+
+    let checks = document.getElementsByClassName("checks")
+
+    for(let i = 0; i < checks.length; i++){
+        let i2 = 1
+        checks[i].onclick = function() {
+            i2++
+            if( i2%2 == 0 ){
+                checks[i].parentElement.classList.add("tdone")
+                checks[i].parentElement.classList.remove("ttodo")
+                all.addEventListener("click", () =>{
+                    checks[i].parentElement.classList.remove("dnone")
+                })
+                todo.addEventListener("click", () =>{
+                    checks[i].parentElement.classList.add("dnone")
+                })
+                done.addEventListener("click", () =>{
+                    checks[i].parentElement.classList.remove("dnone")
+                })
+
+            } else {
+                checks[i].parentElement.classList.add("ttodo")
+                checks[i].parentElement.classList.remove("tdone")
+                all.addEventListener("click", () =>{
+                    checks[i].parentElement.classList.remove("dnone")
+                })
+                todo.addEventListener("click", () =>{
+                    checks[i].parentElement.classList.remove("dnone")
+                })
+                done.addEventListener("click", () =>{
+                    checks[i].parentElement.classList.remove("dnone")
+                })
+            }
+        }
+    }
+
+    let filter = document.getElementsByClassName("new_task")
+
+    for (let i =0; i < filter.length; i++){
+        all.addEventListener("click", () =>{
+            filter[i].classList.remove("dnone")
+        })
+        done.addEventListener("click", () =>{
+            filter[i].classList.add("dnone")
+        })
+
     }
 
     let modify_yourself = document.getElementsByClassName("modify_yourself");
+    let modify_yourself_a = document.getElementsByTagName("p");
 
     for (let i = 0; i < modify_yourself.length; i++) {
         modify_yourself[i].onclick = function() {
@@ -68,13 +122,41 @@ export let create_task = () =>{
                     rename.remove()
                     new_ntp.innerText = rename_value 
                 
+                    }
+                })   
             }
-            })
-            
-    }
-    }
-}
+        }
 
+        modify_yourself_a[i].ondblclick = function() {
+            let this_task = this.parentElement;
+            let this_value = this_task.children[1].innerText
+            this_task.children[1].remove()
+            let rename = document.createElement("input")
+            rename.setAttribute("class", "rename")
+            rename.value = this_value
+            this_task.insertBefore(rename, modify_yourself[i])
+            rename.addEventListener("keydown", (e) =>{
+                if (e.keyCode === 13){
+                    rename.style.display = "none;"
+                    let rename_value = rename.value
+                    let new_ntp = document.createElement("p")
+                    new_ntp.setAttribute("class",".new_task>p")
+                    this_task.insertBefore(new_ntp, modify_yourself[i])
+                    rename.remove()
+                    new_ntp.innerText = rename_value 
+                
+                    }
+                })   
+            }
+        
+    }
+
+    
+    
+        
+
+    
+    
 }
     
 
--- cs-miniprojet-todolist/public/js/main.js
+++ cs-miniprojet-todolist/public/js/main.js
@@ -26,6 +26,7 @@ mode.addEventListener("click", () =>{
 
 
 
+
     
 
     
